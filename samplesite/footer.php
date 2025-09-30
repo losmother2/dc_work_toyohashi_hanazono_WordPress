2a2456239662c6c461d567d762f6738fc01251be
losmother2 <losmother2@gmail.com>
2025-09-30 14:18:40 +0900

    9/30dc word

---
+++ samplesite/footer.php
@@ -0,0 +1,16 @@
+    <footer>
+        <ul>
+            <li class="home"><a href="index.html">TOP</a></li>
+            <li><a href="service.html">SERVICE</a></li>
+            <li><a href="company.html">COMPANY</a></li>
+            <li><a href="contact.html">CONTACT</a></li>
+        </ul>
+        <div id="footer">
+            <div class="copyright">Copyright &copy; 2020 SAMPLE SITE All Rights Reserved.</div>
+        </div>
+    </footer>
+    <div id="pageTop">
+        <a href="#">PAGE TOP</a>
+    </div>
+</body>
+</html>
